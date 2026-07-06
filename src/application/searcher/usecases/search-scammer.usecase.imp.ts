b08298d3a7a736e78727e76af0e1c2e54b96110d
Oscar Cedano <oscar.cedano@outlook.com>
2026-07-06 02:13:38 -0600

    👀 fix: silent error when search scammer usecase request is cancelled

--- src/application/searcher/usecases/search-scammer.usecase.imp.ts
+++ src/application/searcher/usecases/search-scammer.usecase.imp.ts
@@ -1,30 +1,44 @@
 import environment from "@/common/environment";
 import SearchScammerResponse from "@/domain/searcher/models/responses/search-scammer.response";
 import SearchScammerUseCase from "@/domain/searcher/usecases/search-scammer.usecase";
-import PurifierUtil from "@/utils/purifier.util"
+import PurifierUtil from "@/utils/purifier.util";
 import Http from "@/infrastructure/http/http";
 import RequestCanceler from "@/infrastructure/http/request-canceler";
 import ScammerEntity from "@/domain/scammer/entities/scammer.entity";
+import { AxiosResponse } from "axios";
 
 export default class SearchScammerImplUsecase extends SearchScammerUseCase {
-    private readonly requestCanceler = new RequestCanceler();
+  private readonly requestCanceler = new RequestCanceler();
 
-    public async execute(query: string): Promise<ScammerEntity[]> {
-        const signal = this.requestCanceler.prepareSignal();
-        const sanitizedQuery = PurifierUtil.sanitize(query);
+  public async execute(query: string): Promise<ScammerEntity[]> {
+    const signal = this.requestCanceler.prepareSignal();
+    const sanitizedQuery = PurifierUtil.Sanitize(query);
 
-        const response = await Http.get<SearchScammerResponse>(`${environment.API_BASE_URL}/public/scammers/`, {
-            signal,
-            params: {
-                "q": sanitizedQuery
-            }
-        });
+    let response: AxiosResponse<SearchScammerResponse, any, {}> | null = null;
 
-        return response.data.data.map(({id, name, iso_country, is_active}) => 
-            new ScammerEntity(id, name, iso_country, is_active));
+    try {
+      response = await Http.get<SearchScammerResponse>(
+        `${environment.API_BASE_URL}/public/scammers/`,
+        {
+          signal,
+          params: {
+            q: sanitizedQuery,
+          },
+        },
+      );
+    } catch (error) {
+      console.error("SearchScammerUsecase.execute error:", error);
+    } finally {
+      return response == null
+        ? []
+        : response.data.data.map(
+            ({ id, name, iso_country, is_active }) =>
+              new ScammerEntity(id, name, iso_country, is_active),
+          );
     }
+  }
 
-    public cancel(): void {
-        this.requestCanceler.cancel();
-    }
+  public cancel(): void {
+    this.requestCanceler.cancel();
+  }
 }
