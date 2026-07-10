4a54008d6c3875c0c7bbdd26019d55307bdfb948
Oscar Cedano <oscar.cedano@outlook.com>
2026-07-10 15:03:42 -0700

    🛠️  refactor: deleting abstract usecase for searching scammers

--- src/domain/searcher/usecases/search-scammer.usecase.ts
+++
@@ -1,8 +0,0 @@
-import ApiCallerInterface from "@/core/base/api-caller.interface";
-import ScammerEntity from "@/domain/scammer/entities/scammer.entity";
-
-export default abstract class SearchScammerUseCase implements ApiCallerInterface { 
-    public abstract execute(query: string): Promise<ScammerEntity[]>;
-
-    public abstract cancel(): void;
-}
